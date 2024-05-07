6ab8099d811e5b82c3735ccbd68b6060e78233f3
Akın Şibay <sibayakin@gmail.com>
2024-05-07 16:02:42 +0300

    style: renk değiştirildi

--- client/src/components/shared/IdBadge.jsx
+++ client/src/components/shared/IdBadge.jsx
@@ -1,5 +1,6 @@
 import { Badge } from "antd";
 
 export default function IdBadge({ value }) {
-  return <Badge count={value} overflowCount={9999999999} color="rgb(45, 115, 245)" />;
+  return <Badge count={value} overflowCount={9999999999} color="#006d77" />;
+  // return <Badge count={value} overflowCount={9999999999} color="rgb(45, 115, 245)" />;
 }
