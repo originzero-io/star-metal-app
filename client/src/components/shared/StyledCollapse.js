f99ee4dfd396decdc899158115fb224c542296f2
Akın Şibay <sibayakin@gmail.com>
2024-05-21 16:56:48 +0300

    style: minor değişiklikler

--- client/src/components/shared/StyledCollapse.js
+++ client/src/components/shared/StyledCollapse.js
@@ -1,14 +1,13 @@
 const collapseStyle = {
   parentCollapseItem: {
     borderRadius: 10,
-    marginBottom: 6,
+    marginBottom: 8,
     background: "rgba(255, 255, 255, 0.4)",
-    // background: "rgba(161, 46, 134, 0.061)",
   },
   subCollapseItem: {
     borderRadius: 10,
-    marginTop: 4,
-    background: "rgba(255, 255, 255, 0.6)",
+    marginTop: 8,
+    background: "rgba(255, 255, 255, 0.5)",
   },
 };
 
